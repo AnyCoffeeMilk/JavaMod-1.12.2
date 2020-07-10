c9c668c095f8b66f0fd082299652fd07b57cb8e4
Thebloopboop <shinypikasword@gmail.com>
2020-07-10 21:30:49 +0800

    Update ScytheUpgradeContainer.java

--- src/main/java/com/mods/reaper/gui/ScytheUpgradeContainer.java
+++ src/main/java/com/mods/reaper/gui/ScytheUpgradeContainer.java
@@ -1,13 +1,40 @@
 package com.mods.reaper.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
 
 public class ScytheUpgradeContainer extends Container
 {
+	public ScytheUpgradeContainer(InventoryPlayer playerInv, final TileEntityPedestal pedestal) 
+	{
+		IItemHandler inventory = pedestal.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
+		addSlotToContainer(new SlotItemHandler(inventory, 0, 80, 35) 
+		{
+			@Override
+			public void onSlotChanged() {
+				pedestal.markDirty();
+			}
+		});
+	
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 9; j++) {
+				addSlotToContainer(new Slot(playerInv, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+			}
+		}
+	
+		for (int k = 0; k < 9; k++) {
+			addSlotToContainer(new Slot(playerInv, k, 8 + k * 18, 142));
+		}
+	}
+	
 	@Override
     public boolean canInteractWith(EntityPlayer playerIn) 
 	{
